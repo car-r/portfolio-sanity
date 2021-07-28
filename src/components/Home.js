f1f2405d340a45963dd16bbecb47456653492bb8
car-r <carson.ri@protonmail.com>
2021-07-27 21:07:50 -0700

    updated home page and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import image from '../../src/guatemala.jpg'
+import danville from '../../src/danville.jpg'
 
 const Home = () => {
     return (
+        <main className="bg-gray-100">
+            <section className='flex flex-col md:flex-row justify-around items-center py-20 md:w-11/12'>
+                <div className='p-5 md:p-10 pb-10 '>
+                    <h1 className='text-6xl md:text-8xl mb-1 ml-4 text-gray-700'>Hi. I'm Carson</h1>
+                    <h2 className='text-2xl md:text-3xl ml-4 pb-6 text-gray-700'>Insurance professional and front-end developer</h2>
+                    <div className='mb-6 ml-4'>
+                        <a href='https://github.com/car-r' rel="noopener noreferrer" target="_blank"> 
+                            <button className='px-7 py-2 bg-gray-600 text-gray-100 rounded-xl hover:bg-purple-500 focus:bg-purple-300 tracking-widest mr-8'>GitHub</button>
+                        </a>
+                        <a href='https://www.linkedin.com/in/carson-richter-42362132' rel="noopener noreferrer" target="_blank"> 
+                            <button className='px-7 py-2 bg-gray-600 text-gray-100 rounded-xl hover:bg-purple-500 focus:bg-purple-300 tracking-widest'>LinkedIn</button>
+                        </a>
+                    </div>
+                    
+                </div>
+                <img src={danville} alt='carson' className='object-cover home-image'/>
+            </section>
+            
+        </main>
+        /*
         <main>
             <img src={image} alt="" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 sm:p-10">
-                <h1 className="text-6xl text-gray-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Carson</h1>
+                <h1 className="text-8xl text-gray-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Carson</h1>
                 
             </section>
         </main>
+        */
     )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,30 +9,30 @@ const NavBar = () => {
                 <nav className="flex">
                     <NavLink to="/" exact 
                         activeClassName="text-white" 
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-gray-100 hover:text-purple-300 text-4xl font-bold cursive tracking-widest"
+                        className="inflex-flex items-center py-6 px-5 mr-4 text-gray-100 hover:text-purple-300 text-3xl md:text-4xl font-bold cursive tracking-widest"
                     >
                         Carson
                     </NavLink>
                     <NavLink to="/post"
-                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 hover:text-purple-300"
+                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 text-sm md:text-base hover:text-purple-300"
                         activeClassName="text-gray-100 bg-gray-500"
                     >
                         Blog Posts
                     </NavLink>
                     <NavLink to="/project"
-                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 hover:text-purple-300"
+                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 text-sm md:text-base hover:text-purple-300"
                         activeClassName="text-gray-100 bg-gray-500"
                     >
                         Projects
                     </NavLink>
                     <NavLink to="/about"
-                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 hover:text-purple-300"
+                        className="inflex-flex items-center py-3 px-3 my-6 text-gray-100 text-sm md:text-base hover:text-purple-300"
                         activeClassName="text-gray-100 bg-gray-500"
                     >
-                        About Me!
+                        About Me
                     </NavLink>
                 </nav>
-                <div className="inline-flex py-3 px-3 my-6">
+                <div className="inline-flex py-3 px-3 my-6 hidden md:block">
                     <SocialIcon url="https://www.linkedin.com/in/carson-richter-42362132" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}}/>
                 </div>
             </div>
